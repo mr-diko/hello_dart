3c175fe908ea764cee211acf48c8ed804c1cda55
mr.Diko <mr.diko89@gmail.com>
2023-05-03 00:45:39 +0300

    passed the study nullability

--- bin/chellenge.dart
+++ bin/chellenge.dart
@@ -1,15 +1,38 @@
-class Student {
-  final String firstName;
-  final String lastname;
-  int grade;
-  Student(this.firstName, this.lastname, this.grade);
+void main(List<String> args) {
+  final ray = Name(givenName: 'Ray', surname: 'Vanderlih');
+  print(ray);
+}
+
+class Name {
+  Name({
+    required this.givenName,
+    this.surname,
+    this.surnameIsFirst = false,
+  });
+
+  final String givenName;
+  final String? surname;
+  final bool surnameIsFirst;
+
   @override
   String toString() {
-    return '$firstName $lastname: $grade';
+    if (surname == null) {
+      return givenName;
+    }
+    if (surnameIsFirst) {
+      return "$surname $givenName";
+    }
+    return "$givenName $surname";
   }
 }
 
-void main(List<String> args) {
-  final dany = Student('Dany', 'Dodik', 22);
-  print(dany);
-}
+// class Student {
+//   final String firstName;
+//   final String lastname;
+//   int grade;
+//   Student(this.firstName, this.lastname, this.grade);
+//   @override
+//   String toString() {
+//     return '$firstName $lastname: $grade';
+//   }
+// }
