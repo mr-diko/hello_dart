b9e609b8f1d935cd86aad650d4f74c4fd0f6a9dd
mr.Diko <mr.diko89@gmail.com>
2023-05-20 12:35:34 +0300

    git

--- bin/lists.dart
+++ bin/lists.dart
@@ -1,21 +1,7 @@
-var months = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-];
 void main(List<String> args) {
-  months.add('December');
-  var marchIndex = months.indexOf('March');
-  months.removeAt(marchIndex);
-  months.insert(marchIndex, 'March');
-
-  print(months);
+  for (var i = 0; i < numbers.length; i++) {
+    print(numbers[i] * numbers[i]);
+  }
 }
+
+const numbers = [1, 2, 4, 7];
